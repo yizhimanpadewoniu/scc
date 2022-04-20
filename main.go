fbb8577c280fc31c396a8232ca07fa48918a396f
yizhimanpadewoniu@yeah.net <yizhimanpadewoniu@yeah.net>
2022-04-21 00:40:58 +0800

    update dev

--- main.go
+++ main.go
@@ -1,8 +1,9 @@
 package main
 
 import (
+	"fmt"
 	"github.com/gin-gonic/gin"
-	"reflect"
+	"net/http"
 )
 
 func main() {
@@ -10,8 +11,17 @@ func main() {
 	//r.GET("/", func(c *gin.Context) {
 	//	c.String(200, "Hello, Geektutu")
 	//})
-	r.POST("/gitlabscanner", func(context *gin.Context) {
-		println(reflect.TypeOf(context))
-	})
+	r.POST("/gitlabscanner", events)
 	r.Run() // listen and serve on 0.0.0.0:8080
+}
+
+func events(c *gin.Context) {
+	buf := make([]byte, 1024)
+	n, _ := c.Request.Body.Read(buf)
+	fmt.Println(string(buf[0:n]))
+	resp := map[string]string{"hello": "world"}
+	c.JSON(http.StatusOK, resp)
+	/*post_gwid := c.PostForm("name")
+	  fmt.Println(post_gwid)*/
+
 }
